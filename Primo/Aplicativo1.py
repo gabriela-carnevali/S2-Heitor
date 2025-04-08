88feb0518870531532bc7aaad0be9c9ecd0d995c
Heitor-09 <heitor.b.santos6@aluno.senai.br>
2025-04-08 09:40:17 -0300

    Primo 1
    certo

---
+++ Primo/Aplicativo1.py
@@ -0,0 +1,13 @@
+print("Olá usuário, me de um número e eu vou ver se ele é primo ou não")
+num = int(input("Me de um número"))
+def primo(num):
+    for pares in range(2, num):
+        if num % pares == 0:
+            print("O número", num, "não é primo")
+            break
+        else:
+            print("O número", num, "é primo")
+            break
+primo(num)
+
+
