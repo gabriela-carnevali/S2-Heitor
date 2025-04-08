dde38fec788d5f9c6b8b0cbda4bc90f453670af4
Heitor-09 <heitor.b.santos6@aluno.senai.br>
2025-04-08 09:35:03 -0300

    Main
    erro

---
+++ Primo/Aplicativo1.py
@@ -0,0 +1,6 @@
+print("Olá usuário, me de um número e eu vou ver se ele é primo ou não")
+num = int(input("Me de um número"))
+def primo(num):
+    for pares in range(2, num):
+        if num % pares == 0:
+            print("O número", num, "não é primo")
